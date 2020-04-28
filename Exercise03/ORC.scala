8726239d94fdbbe36612262a2805ff299550ad6c
adityashah95 <43364258+adityashah95@users.noreply.github.com>
2020-04-28 17:58:50 +0530

    Update ORC.scala

--- Exercise03/ORC.scala
+++ Exercise03/ORC.scala
@@ -1,8 +1,8 @@
 // Read CSV
-var df_census_csv = spark.read.options(Map("inferSchema"->"true","delimiter"->",","header"->"true")).csv("F:/Chapter06/Data/Census.csv")
+var df_census_csv = spark.read.options(Map("inferSchema"->"true","delimiter"->",","header"->"true")).csv("F:/Data/Census.csv")
 
 // Read JSON
-var df_census_json = spark.read.json("F:/Chapter06/Data/Census.json")
+var df_census_json = spark.read.json("F:/Data/Census.json")
 
 // Show the df
 df_census_csv.show()
@@ -12,12 +12,12 @@ df_census_json.show()
 //  Writing to ORC
 
 // Using CSV Data frame
-df_census_csv.write.orc("F:/Chapter06/Data/Output/census_csv.orc")
+df_census_csv.write.orc("F:/Data/Output/census_csv.orc")
 
 // Using JSON Data frame
-df_census_json.write.orc("F:/Chapter06/Output/Data/census_json.orc")
+df_census_json.write.orc("F:/Output/Data/census_json.orc")
 
 
 // Reading ORC file
-val df_census_orc = spark.read.orc("F:/Chapter06/Data/Output/census_csv.orc")
+val df_census_orc = spark.read.orc("F:/Data/Output/census_csv.orc")
 df_census_orc.show()
